3b516072fae6153873cfcd7cf6eb4365a2576180
longNH <nguyenhailong270921@gmail.com>
2023-04-11 11:41:54 +0700

    [Feat: change ui manage campaing modal]

--- frontend/src/services/idea.services.js
+++ frontend/src/services/idea.services.js
@@ -13,16 +13,16 @@ export async function getIdeaById(payload) {
     return await http.get(`/idea/${payload?.id}`);
 }
 
-export async function getIdeaMostLike() {
-    return await http.get('/ideaMostReaction');
+export async function getIdeaMostLike(payload) {
+    return await http.post('/idea/mostLike', payload);
 }
 
-export async function getIdeasMostView() {
-    return await http.get('/ideaMostView');
+export async function getIdeasMostView(payload) {
+    return await http.post('/idea/mostView', payload);
 }
 
-export async function getIdeasLatest() {
-    return await http.get('/ideaLatest')
+export async function getIdeasLatest(payload) {
+    return await http.post('/idea/ideaLatest')
 }
 
 export async function putIdea(id, payload) {
